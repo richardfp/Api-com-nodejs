2585036082fb6b24f21ad18ce39e01f895ef43cc
Richard <w3_lfabio@banparanet.com.br>
2022-10-21 14:15:34 -0300

    first commi

---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+const express = require('express');
+const exp = require('constants');
+
+const app = express();
+const port = 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node Store Api",
+        version: "0.0.1"
+    });
+});
+app.use('/', route);
+
+server.listen(port);
+console.log('API rodando na porta'+ port);
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >=0) {
+        return port
+    }
+    return false;
+}
